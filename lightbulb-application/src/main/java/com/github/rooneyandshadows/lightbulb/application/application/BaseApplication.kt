f17bce0ab726c706553a9c7337cbd69c055b545d
rooney123777 <rooney123777@gmail.com>
2022-01-24 17:23:27 +0200

    version 1.0.20 release - Configuration update on application context when locale changed.

--- lightbulb-application/src/main/java/com/github/rooneyandshadows/lightbulb/application/application/BaseApplication.kt
+++ lightbulb-application/src/main/java/com/github/rooneyandshadows/lightbulb/application/application/BaseApplication.kt
@@ -3,6 +3,7 @@ package com.github.rooneyandshadows.lightbulb.application.application
 import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
+import com.github.rooneyandshadows.lightbulb.commons.utils.LocaleHelper
 
 abstract class BaseApplication : Application() {
 
@@ -24,5 +25,9 @@ abstract class BaseApplication : Application() {
         super.onCreate()
         application = this
         create()
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(LocaleHelper.onAttach(base))
     }
 }
