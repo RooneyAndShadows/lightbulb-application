57e0f26a141ebc51c7f1ee68e5f67f018b02c086
rooney123777 <rooney123777@gmail.com>
2022-11-05 23:43:47 +0200

    .

--- annotation-processors/src/main/java/com/github/rooneyandshadows/lightbulb/annotation_processors/FragmentProcessor.java
+++ annotation-processors/src/main/java/com/github/rooneyandshadows/lightbulb/annotation_processors/FragmentProcessor.java
@@ -6,6 +6,8 @@ import com.google.auto.service.AutoService;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
@@ -28,6 +30,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
 
@@ -61,6 +64,7 @@ public class FragmentProcessor extends AbstractProcessor {
                 return false;
             }
             ClassInfo classInfo = new ClassInfo();
+            classInfo.setType(element.asType());
             classInfo.setSimpleClassName(element.getSimpleName().toString());
             classInfo.setFullClassName(getFullClassName(elements, element));
             classInfo.configAnnotation = element.getAnnotation(FragmentConfiguration.class);
@@ -75,28 +79,18 @@ public class FragmentProcessor extends AbstractProcessor {
                     .orElse(null);
             if (classInfo == null) {
                 classInfo = new ClassInfo();
+                classInfo.setType(classElement.asType());
                 classInfo.setSimpleClassName(classElement.getSimpleName().toString());
                 informationList.add(classInfo);
             }
             classInfo.viewBindings.put(element.getSimpleName().toString(), annotation.name());
-
-
-            //informationList.stream().filter(new Predicate<ClassInfo>() {
-            //    @Override
-            //    public boolean test(ClassInfo classInfo) {
-            //        classInfo.className == className.
-            //    }
-            //})
-
-
         }
-        messager.printMessage(Diagnostic.Kind.ERROR, String.valueOf(informationList.size()));
+
         /**
          *      2) For each annotated class, generate new static method
          */
 
         List<MethodSpec> methods = new ArrayList<>();
-
         informationList.forEach(classInfo -> {
             boolean hasFragmentConfigAnnotation = classInfo.configAnnotation != null;
             String simpleName = classInfo.simpleClassName;
@@ -106,14 +100,25 @@ public class FragmentProcessor extends AbstractProcessor {
                 String isMainScreenFragment = String.valueOf(classInfo.configAnnotation.isMainScreenFragment());
                 String hasLeftDrawer = String.valueOf(classInfo.configAnnotation.hasLeftDrawer());
                 String hasOptionsMenu = String.valueOf(classInfo.configAnnotation.hasOptionsMenu());
-                MethodSpec generateFragmentConfigMethod = MethodSpec
-                        .methodBuilder("generateFragmentConfiguration" + simpleName)
+                MethodSpec fragConfigMethod = MethodSpec
+                        .methodBuilder("generate" + simpleName + "Configuration")
                         .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                         .returns(FragmentConfig.class)
                         .addStatement("return new $T($S,$L,$L,$L)", fragConfigClassName, layoutName, isMainScreenFragment, hasLeftDrawer, hasOptionsMenu)
                         .build();
-                methods.add(generateFragmentConfigMethod);
+                methods.add(fragConfigMethod);
             }
+            //messager.printMessage(Diagnostic.Kind.ERROR, classInfo.fullClassName);
+            MethodSpec.Builder fragViewBindingMethod = MethodSpec
+                    .methodBuilder("generate" + simpleName + "ViewBindings")
+                    .addParameter(TypeName.get(classInfo.type), "fragment")
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class);
+            classInfo.viewBindings.forEach((fieldName, identifierName) -> {
+                fragViewBindingMethod.addStatement("fragment.$L = fragment.getView().findViewById(fragment.getResources().getIdentifier($S, \"id\", fragment.getActivity().getPackageName()))", fieldName, identifierName);
+            });
+            methods.add(fragViewBindingMethod.build());
+
         });
 
 
@@ -172,10 +177,15 @@ public class FragmentProcessor extends AbstractProcessor {
     }
 
     private static class ClassInfo {
+        private TypeMirror type;
         private String simpleClassName;
         private String fullClassName;
         private FragmentConfiguration configAnnotation;
         private final Map<String, String> viewBindings = new HashMap<>();
+
+        public TypeMirror getType() {
+            return type;
+        }
 
         public String getSimpleClassName() {
             return simpleClassName;
@@ -191,6 +201,10 @@ public class FragmentProcessor extends AbstractProcessor {
 
         public Map<String, String> getViewBindings() {
             return viewBindings;
+        }
+
+        public void setType(TypeMirror type) {
+            this.type = type;
         }
 
         public void setSimpleClassName(String simpleClassName) {
