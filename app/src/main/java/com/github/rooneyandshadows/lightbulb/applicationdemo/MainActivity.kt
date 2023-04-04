fdbff7b20cc7e2c1c517e9d874a7122c1c1ba2be
rooney123777 <rooney123777@gmail.com>
2023-04-04 19:00:23 +0300

    version 1.4.0 release - Dependency updates.

--- app/src/main/java/com/github/rooneyandshadows/lightbulb/applicationdemo/MainActivity.kt
+++ app/src/main/java/com/github/rooneyandshadows/lightbulb/applicationdemo/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.github.rooneyandshadows.lightbulb.application
+package com.github.rooneyandshadows.lightbulb.applicationdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.rooneyandshadows.lightbulb.application.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R
-            .layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
